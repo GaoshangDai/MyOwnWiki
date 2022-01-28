b25ccb3248434077b6daf7a0db8096d6d35cbb0f
Gaoshang Dai <gdai@ucsd.edu>
2022-01-28 00:36:02 -0800

    init

---
+++ src/main/java/com/gsdai/myownwiki/MyOwnWikiApplication.java
@@ -0,0 +1,13 @@
+package com.gsdai.myownwiki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyOwnWikiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyOwnWikiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gsdai/myownwiki/MyOwnWikiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gsdai.myownwiki;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyOwnWikiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
