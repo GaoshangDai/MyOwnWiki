deb7fa96c349b970f873f4dcd6855d07b03bad47
Gaoshang Dai <gdai@ucsd.edu>
2022-02-02 16:32:30 -0800

    integrate with mybatis generator and generate code of "demo" table

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -7,3 +7,12 @@ create table `test` (
 ) engine=innodb default charset=utf8mb4;
 
 insert into `test` (id, name, password) values (1, 'test', 'password');
+
+drop table if exists `demo`;
+create table `demo` (
+                        `id` bigint not null,
+                        `name` varchar(50),
+                        primary key (`id`)
+) engine=innodb default charset=utf8mb4;
+
+insert into `demo` (id, name) values (1, 'test');
