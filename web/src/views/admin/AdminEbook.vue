ce2390eb3532a4562b1aa951aa8a655a00062957
Gaoshang Dai <gdai@ucsd.edu>
2022-02-17 18:20:02 -0800

    add modal

--- web/src/views/admin/AdminEbook.vue
+++ web/src/views/admin/AdminEbook.vue
@@ -4,50 +4,58 @@
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
     >
       <a-table
-        :columns="columns"
-        :row-key="record => record.id"
-        :data-source="ebooks"
-        :pagination="pagination"
-        :loading="loading"
-        @change="handleTableChange"
-    >
-      <template #cover="{ text: cover }">
-        <img v-if="cover" :src="cover" alt="avatar" />
-      </template>
-      <template v-slot:action="{ text, record }">
-        <a-space size="small">
-          <a-button type="primary">
-            Edit
-          </a-button>
-          <a-button type="danger">
-            Delete
-          </a-button>
-        </a-space>
-      </template>
-    </a-table>
+          :columns="columns"
+          :row-key="record => record.id"
+          :data-source="ebooks"
+          :pagination="pagination"
+          :loading="loading"
+          @change="handleTableChange"
+      >
+        <template #cover="{ text: cover }">
+          <img v-if="cover" :src="cover" alt="avatar"/>
+        </template>
+        <template v-slot:action="{ text, record }">
+          <a-space size="small">
+            <a-button type="primary" @click="edit">
+              Edit
+            </a-button>
+            <a-button type="danger">
+              Delete
+            </a-button>
+          </a-space>
+        </template>
+      </a-table>
     </a-layout-content>
   </a-layout>
+  <a-modal
+      title="Ebooks Table"
+      v-model:visible="modalVisible"
+      :confirm-loading="modalLoading"
+      @ok="handleModalOk"
+  >
+    <p>Test</p>
+  </a-modal>
 </template>
 
 <script lang="ts">
-import { defineComponent, onMounted, ref } from 'vue';
-import axios from 'axios';
+import {defineComponent, onMounted, ref} from 'vue'
+import axios from 'axios'
 
 export default defineComponent({
   name: 'AdminEbook',
   setup() {
-    const ebooks = ref();
+    const ebooks = ref()
     const pagination = ref({
       current: 1,
       pageSize: 5,
       total: 0
-    });
-    const loading = ref(false);
+    })
+    const loading = ref(false)
 
     const columns = [
       {
         dataIndex: 'cover',
-        slots: { customRender: 'cover' }
+        slots: {customRender: 'cover'}
       },
       {
         title: 'Name',
@@ -76,50 +84,68 @@ export default defineComponent({
       },
       {
         key: 'action',
-        slots: { customRender: 'action' }
+        slots: {customRender: 'action'}
       }
-    ];
+    ]
 
     const handleQuery = (params: any) => {
-      loading.value = true;
+      loading.value = true
       axios.get("/ebook/list", {
         params: {
           page: params.page,
           size: params.size
         }
       }).then((response) => {
-        loading.value = false;
-        const data = response.data;
-        ebooks.value = data.content.list;
+        loading.value = false
+        const data = response.data
+        ebooks.value = data.content.list
 
-        pagination.value.current = params.page;
-        pagination.value.total = data.content.total;
-      });
-    };
+        pagination.value.current = params.page
+        pagination.value.total = data.content.total
+      })
+    }
 
     const handleTableChange = (pagination: any) => {
       handleQuery({
         page: pagination.current,
         size: pagination.pageSize
-      });
-    };
+      })
+    }
+
+    const modalVisible = ref(false)
+    const modalLoading = ref(false)
+    const handleModalOk = () => {
+      modalLoading.value = true
+      setTimeout(() => {
+        modalVisible.value = false
+        modalLoading.value = false
+      }, 2000)
+    }
+
+    const edit = () => {
+      modalVisible.value = true
+    }
 
     onMounted(() => {
       handleQuery({
         page: 1,
         size: pagination.value.pageSize
-      });
-    });
+      })
+    })
 
     return {
       ebooks,
       pagination,
       columns,
       loading,
-      handleTableChange
+      handleTableChange,
+      edit,
+      modalVisible,
+      modalLoading,
+      handleModalOk
     }
   }
-});
+})
 </script>
 
 <style scoped>
